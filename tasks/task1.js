6289dab5019f6ba60ea24e4cbc1d6f99b473f8d9
kn24-mrytchenko-prog <kn24-m.rytchenko@nubip.edu.ua>
2025-10-27 15:57:36 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,7 +1,6 @@
-//Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
-//вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
 function filterAdults(people) {
-  //Ваш код
+  // Використаємо метод filter для створення нового масиву
+  return people.filter(person => person.age >= 18);
 }
 
 module.exports = filterAdults;
